2f954a950539383caefd9e3461112e064b2ff907
B Sanchit Smarak <64328578+bsanchit@users.noreply.github.com>
2021-06-21 21:19:54 +0530

    Add files via upload

---
+++ ShapeAi Weather.py
@@ -0,0 +1,31 @@
+import requests
+import os
+from datetime import datetime
+import sys
+
+api_key = 'c0dfab4f73f1df2016e7f495270e6d91'
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+#create variables to store and display data
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+sys.stdout = open("text.txt", "w")
+print ("..............................................................")
+print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("..............................................................")
+
+print ("Current temperature is: {:.2f} deg C".format(temp_city))
+print ("Current weather desc  :",weather_desc)
+print ("Current Humidity      :",hmdt, '%')
+print ("Current wind speed    :",wind_spd ,'kmph')
+
+sys.stdout.close()
+
